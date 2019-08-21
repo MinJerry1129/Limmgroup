4d7b210248e730138e41ec84030dd13dd99726b5
Admin <admin@Mac-Admin.local>
2019-08-21 16:48:49 +0300

    Initial Commit

---
+++ woocomence/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  woocomence
+//
+//  Created by Admin on 8/21/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
